161eb74bb6c3456c5fdaa4ecc4f22c1cf2000ab3
ravi2krishna <ravi2krishna@gmail.com>
2023-10-25 04:27:55 +0000

    update public ip

--- vpc.tf
+++ vpc.tf
@@ -13,6 +13,7 @@ resource "aws_subnet" "tf-ecomm-pub-sn-A" {
   vpc_id     = aws_vpc.tf-ecomm.id
   cidr_block = "192.168.0.0/24"
   availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"
   tags = {
     Name = "ecomm-public-subnet-A"
   }
