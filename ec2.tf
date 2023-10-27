11d9edff66604b2723c9fcafc8219e6ddde42dee
ravi2krishna <ravi2krishna@gmail.com>
2023-10-27 04:20:10 +0000

    secuirty group

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+# Security Group
+resource "aws_security_group" "tf-ecomm-pub-sg" {
+  name        = "ecomm-web-sg"
+  description = "Allow SSH & HTTP traffic"
+  vpc_id      = aws_vpc.tf-ecomm.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecomm-web-sg"
+  }
+}
+
