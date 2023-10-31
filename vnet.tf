dd4cdafa888f085bc1ba6a69ffc6a377e9b90d94
ravi2krishna <ravi2krishna@gmail.com>
2023-10-31 03:56:17 +0000

    create vnet

--- vnet.tf
+++ vnet.tf
@@ -6,3 +6,11 @@ resource "azurerm_resource_group" "tf-ecomm" {
     env = "dev"
   }
 }
+
+# Virtual Network
+resource "azurerm_virtual_network" "tf-ecomm-vnet" {
+  name                = "ecomm-network"
+  location            = azurerm_resource_group.tf-ecomm.location
+  resource_group_name = azurerm_resource_group.tf-ecomm.name
+  address_space       = ["10.0.0.0/16"]
+}
