7e094f0e6f11ac5908865958da518f5fe4b46f19
ravi2krishna <ravi2krishna@gmail.com>
2023-10-31 04:26:56 +0000

    db nic

--- vnet.tf
+++ vnet.tf
@@ -132,3 +132,16 @@ resource "azurerm_network_interface" "tf-ecomm-web-nic" {
     public_ip_address_id          = azurerm_public_ip.tf-ecomm-web-pip.id
   }
 }
+
+# Databse Network Interface
+resource "azurerm_network_interface" "tf-ecomm-db-nic" {
+  name                = "db-nic"
+  location            = azurerm_resource_group.tf-ecomm.location
+  resource_group_name = azurerm_resource_group.tf-ecomm.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.tf-ecomm-db-sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
