5ee8c8b259784bfde16a3f89e867096bed7b4fe5
ravi2krishna <ravi2krishna@gmail.com>
2023-10-31 03:58:58 +0000

    web subnet

--- vnet.tf
+++ vnet.tf
@@ -14,3 +14,11 @@ resource "azurerm_virtual_network" "tf-ecomm-vnet" {
   resource_group_name = azurerm_resource_group.tf-ecomm.name
   address_space       = ["10.0.0.0/16"]
 }
+
+# Web Subnet
+resource "azurerm_subnet" "tf-ecomm-web-sn" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.tf-ecomm.name
+  virtual_network_name = azurerm_virtual_network.tf-ecomm-vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
